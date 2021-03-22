da6a02a036ed644068bdf5ae6b343180478ed91d
SFMack <sfcamack@icloud.com>
2021-03-21 21:28:05 -0700

    pg database configured. members table created. one member added to the table for test. need to add foreign key to members table

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
 const express = require("express");
 const cors = require("cors");
+const knex = require("knex");
+require("dotenv").config();
 
 const app = express();
 
-app.get("/", (req, res) => res.send("Home"));
-
 const port = process.env.PORT || 5000;
+
+const db = knex({
+  client: "pg",
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB,
+  },
+});
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use(cors());
+
+// GET Fetch all members from the database
+app.get("/", (req, res) => {
+  db.select("*")
+    .from("members")
+    .then((member) => {
+      console.log(member);
+      res.status(201).json(member);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+});
+
+app.get("/", (req, res) => res.send("Home"));
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
