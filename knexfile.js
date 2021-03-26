c340f8c7ec835ebf7166d44b4906005f78083eda
SFMack <sfcamack@icloud.com>
2021-03-26 14:44:00 -0700

    knexfile update

--- knexfile.js
+++ knexfile.js
@@ -46,11 +46,15 @@ module.exports = {
   production: {
     client: "pg",
     connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10,
+    },
     migrations: {
-      directory: __dirname + "/db/migrations",
+      directory: "/db/migrations",
     },
     seeds: {
-      directory: __dirname + "/db/seeds",
+      directory: "/db/seeds",
     },
     // pool: {
     //   afterCreate: (conn, done) => {
