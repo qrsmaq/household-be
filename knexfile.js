ac51c498d3145c0592e96bb377a3d1fff722cc54
SFMack <sfcamack@icloud.com>
2021-03-26 13:52:48 -0700

    knexfile change

--- knexfile.js
+++ knexfile.js
@@ -40,45 +40,45 @@ module.exports = {
     },
   },
 
-  // production: {
-  //   client: "pg",
-  //   connection: {
-  //     database: "my_db",
-  //     user: "username",
-  //     password: "password",
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10,
-  //   },
-  //   migrations: {
-  //     directory: __dirname + "/db/migrations",
-  //     // tableName: "knex_migrations",
-  //   },
-  //   seeds: {
-  //     directory: __dirname + "/db/seeds",
-  //   },
-  // },
   production: {
     client: "pg",
     connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB,
+      database: "my_db",
+      user: "username",
+      password: "password",
     },
-    useNullAsDefault: true,
+    pool: {
+      min: 2,
+      max: 10,
+    },
     migrations: {
       directory: __dirname + "/db/migrations",
+      // tableName: "knex_migrations",
     },
     seeds: {
       directory: __dirname + "/db/seeds",
     },
-    // pool: {
-    //   afterCreate: (conn, done) => {
-    //     // runs after a connection is made to the sqlite engine
-    //     conn.run("PRAGMA foreign_keys = ON", done); // turn on FK enforcement
-    //   },
-    // },
   },
+  // production: {
+  //   client: "pg",
+  //   connection: {
+  //     host: process.env.DB_HOST,
+  //     user: process.env.DB_USERNAME,
+  //     password: process.env.DB_PASSWORD,
+  //     database: process.env.DB,
+  //   },
+  //   useNullAsDefault: true,
+  //   migrations: {
+  //     directory: __dirname + "/db/migrations",
+  //   },
+  //   seeds: {
+  //     directory: __dirname + "/db/seeds",
+  //   },
+  //   // pool: {
+  //   //   afterCreate: (conn, done) => {
+  //   //     // runs after a connection is made to the sqlite engine
+  //   //     conn.run("PRAGMA foreign_keys = ON", done); // turn on FK enforcement
+  //   //   },
+  //   // },
+  // },
 };
