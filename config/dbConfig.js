c54349c67cb842d6eadc3f099190b740e9869d4c
SFMack <sfcamack@icloud.com>
2021-03-26 15:33:23 -0700

    knexfile updates and cleanup'

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,9 +1,9 @@
 const knex = require("knex");
 
-const config = require("../knexfile.js");
+const config_file = require("../knexfile.js");
 
 const DB_ENV = process.env.DATABASE_ENV || "development";
 
-const db = knex(config[DB_ENV]);
+const db = knex(config_file[DB_ENV]);
 
 module.exports = db;
--- knexfile.js
+++ knexfile.js
@@ -9,40 +9,23 @@ module.exports = {
       password: process.env.DATABASE_PASSWORD,
       database: process.env.DATABASE_URL,
     },
+    pool: {
+      afterCreate: (conn, done) => {
+        // runs after a connection is made to the sqlite engine
+        conn.run("PRAGMA foreign_keys = ON", done);
+      },
+    },
     useNullAsDefault: true,
     migrations: {
       directory: __dirname + "/db/migrations",
+      tableName: "dbmigrations",
     },
     seeds: {
       directory: __dirname + "/db/seeds",
+      tableName: "dbseeds",
     },
-    // pool: {
-    //   afterCreate: (conn, done) => {
-    //     // runs after a connection is made to the sqlite engine
-    //     conn.run("PRAGMA foreign_keys = ON", done); // turn on FK enforcement
-    //   },
-    // },
   },
 
-  // production: {
-  //   client: "pg",
-  //   connection: {
-  //     database: "my_db",
-  //     user: "username",
-  //     password: "password",
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10,
-  //   },
-  //   migrations: {
-  //     directory: __dirname + "/db/migrations",
-  //     // tableName: "knex_migrations",
-  //   },
-  //   seeds: {
-  //     directory: __dirname + "/db/seeds",
-  //   },
-  // },
   production: {
     client: "pg",
     connection: process.env.DATABASE_URL,
@@ -56,11 +39,5 @@ module.exports = {
     seeds: {
       directory: __dirname + "/db/seeds",
     },
-    // pool: {
-    //   afterCreate: (conn, done) => {
-    //     // runs after a connection is made to the sqlite engine
-    //     conn.run("PRAGMA foreign_keys = ON", done); // turn on FK enforcement
-    //   },
-    // },
   },
 };
