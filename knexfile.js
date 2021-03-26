60c75c9613ddce490432ce85cb8bd7e6f7996cb5
SFMack <sfcamack@icloud.com>
2021-03-26 14:17:16 -0700

    postgres updates

--- knexfile.js
+++ knexfile.js
@@ -4,10 +4,10 @@ module.exports = {
   development: {
     client: "pg",
     connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB,
+      host: process.env.DATABASE_HOST,
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_URL,
     },
     useNullAsDefault: true,
     migrations: {
@@ -24,61 +24,45 @@ module.exports = {
     // },
   },
 
-  staging: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password",
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: "knex_migrations",
-    },
-  },
-
-  production: {
-    client: "pg",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password",
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      directory: __dirname + "/db/migrations",
-      // tableName: "knex_migrations",
-    },
-    seeds: {
-      directory: __dirname + "/db/seeds",
-    },
-  },
   // production: {
   //   client: "pg",
   //   connection: {
-  //     host: process.env.DB_HOST,
-  //     user: process.env.DB_USERNAME,
-  //     password: process.env.DB_PASSWORD,
-  //     database: process.env.DB,
+  //     database: "my_db",
+  //     user: "username",
+  //     password: "password",
   //   },
-  //   useNullAsDefault: true,
+  //   pool: {
+  //     min: 2,
+  //     max: 10,
+  //   },
   //   migrations: {
   //     directory: __dirname + "/db/migrations",
+  //     // tableName: "knex_migrations",
   //   },
   //   seeds: {
   //     directory: __dirname + "/db/seeds",
   //   },
-  //   // pool: {
-  //   //   afterCreate: (conn, done) => {
-  //   //     // runs after a connection is made to the sqlite engine
-  //   //     conn.run("PRAGMA foreign_keys = ON", done); // turn on FK enforcement
-  //   //   },
-  //   // },
   // },
+  production: {
+    client: "pg",
+    connection: {
+      host: process.env.DATABASE_HOST,
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_URL,
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: __dirname + "/db/migrations",
+    },
+    seeds: {
+      directory: __dirname + "/db/seeds",
+    },
+    // pool: {
+    //   afterCreate: (conn, done) => {
+    //     // runs after a connection is made to the sqlite engine
+    //     conn.run("PRAGMA foreign_keys = ON", done); // turn on FK enforcement
+    //   },
+    // },
+  },
 };
